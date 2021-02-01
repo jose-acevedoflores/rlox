85db8c176d257b4ecbdaccd4ec467913fd6a214e
Jose Acevedo <acevedoflores.jose@gmail.com>
2021-01-31 18:15:47 -0800

    Chapter 5 clean up types

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs::File;
+use std::io;
 use std::io::Write;
 
 fn generate_ast() -> String {
@@ -15,7 +16,9 @@ fn generate_ast() -> String {
     args.pop().unwrap()
 }
 
-fn define_ast(out_dir: &String, base_name: &str, types: Vec<String>) {
+type IORes = io::Result<usize>;
+
+fn define_ast(out_dir: &String, base_name: &str, types: &Vec<GrammarTy>) -> IORes {
     let p = std::path::PathBuf::from(out_dir);
     let p = p.join(std::path::PathBuf::from(format!(
         "{}.rs",
@@ -26,102 +29,87 @@ fn define_ast(out_dir: &String, base_name: &str, types: Vec<String>) {
 
     f.write("use shared::tokens;\n".as_bytes()).unwrap();
 
-    define_enum_type(&mut f, base_name, &types);
+    define_enum_type(&mut f, base_name, &types)?;
 
-    define_struct_types(&mut f, base_name, &types);
+    define_struct_types(&mut f, base_name, &types)?;
 
-    define_accept_trait(&mut f, base_name, &types);
+    define_accept_trait(&mut f, base_name, &types)?;
 
-    define_visitor(&mut f, base_name, &types);
+    define_visitor(&mut f, base_name, &types)
 }
 
-fn define_accept_trait(file: &mut File, base_name: &str, types: &Vec<String>) {
+fn define_accept_trait(file: &mut File, base_name: &str, types: &Vec<GrammarTy>) -> IORes {
     file.write(
         "pub trait Accept<R> {\n    fn accept(&self, visitor: &dyn Visitor<R>) -> R;\n}\n"
             .as_bytes(),
-    )
-    .unwrap();
+    )?;
 
-    file.write(format!("impl<R> Accept<R> for {} {{\n", base_name).as_bytes())
-        .unwrap();
-    file.write("    fn accept(&self, visitor: &dyn Visitor<R>) -> R {\n".as_bytes())
-        .unwrap();
-    file.write("        match self {\n".as_bytes()).unwrap();
+    file.write(format!("impl<R> Accept<R> for {} {{\n", base_name).as_bytes())?;
+    file.write(
+        format!(
+            "{:4}fn accept(&self, visitor: &dyn Visitor<R>) -> R {{\n",
+            " "
+        )
+        .as_bytes(),
+    )?;
+    file.write(format!("{:8}match self {{\n", " ").as_bytes())?;
 
     for typ in types {
-        let mut s = typ.split(":");
-        let class_name = s.next().unwrap().trim();
         file.write(
             format!(
-                "{}::{}(a) => visitor.visit_{}_{}(a),\n",
+                "{:12}{}::{}(a) => visitor.visit_{}_{}(a),\n",
+                " ",
                 base_name,
-                class_name,
-                class_name.to_lowercase(),
+                typ.class_name,
+                typ.class_name.to_lowercase(),
                 base_name.to_lowercase(),
             )
             .as_bytes(),
-        )
-        .unwrap();
+        )?;
     }
 
-    file.write("        }\n    }\n}\n".as_bytes()).unwrap();
+    file.write(format!("{:8}}}\n{:4}}}\n}}\n", " ", " ").as_bytes())
 }
 
-fn define_enum_type(file: &mut File, base_name: &str, types: &Vec<String>) {
-    file.write(format!("pub enum {} {{ \n", base_name).as_bytes())
-        .unwrap();
-    types.iter().for_each(|ty| {
-        let mut s = ty.split(":");
-        let class_name = s.next().unwrap().trim();
-        file.write(format!("    {}({}),\n", class_name, class_name).as_bytes())
-            .unwrap();
-    });
-    file.write("} \n".as_bytes()).unwrap();
-}
-
-fn define_struct_types(file: &mut File, base_name: &str, types: &Vec<String>) {
-    for typ in types {
-        let mut s = typ.split(":");
-        let class_name = s.next().unwrap().trim();
-        let fields = s.next().unwrap().trim();
-
-        file.write(format!("pub struct {} {{\n", class_name).as_bytes())
-            .unwrap();
-
-        fields.split(",").for_each(|field| {
-            let mut s = field.trim().split(" ");
-            let ty = s.next().unwrap().trim();
-            let name = s.next().unwrap().trim();
-
-            let ty = ty_map::map_to_rs_type(ty, base_name);
-
-            file.write(format!("    pub {}: {},\n", name, ty).as_bytes())
-                .unwrap();
-        });
-        file.write("}\n".as_bytes()).unwrap();
-    }
-}
-
-fn define_visitor<'a>(file: &mut File, base_name: &str, types: &Vec<String>) {
-    file.write(format!("pub trait Visitor<R> {{\n").as_bytes())
-        .unwrap();
+fn define_enum_type(file: &mut File, base_name: &str, types: &Vec<GrammarTy>) -> IORes {
+    file.write(format!("pub enum {} {{ \n", base_name).as_bytes())?;
     for ty in types {
-        let mut s = ty.split(":");
-        let class = s.next().unwrap().trim();
+        file.write(format!("    {}({}),\n", ty.class_name, ty.class_name).as_bytes())?;
+    }
+    file.write("} \n".as_bytes())
+}
 
+fn define_struct_types(file: &mut File, base_name: &str, types: &Vec<GrammarTy>) -> IORes {
+    for typ in types {
+        let fields = &typ.fields;
+
+        file.write(format!("pub struct {} {{\n", typ.class_name).as_bytes())?;
+
+        for field in fields {
+            let ty = ty_map::map_to_rs_type(field.ty, base_name);
+            file.write(format!("{:4}pub {}: {},\n", " ", field.name, ty).as_bytes())?;
+        }
+        file.write("}\n".as_bytes())?;
+    }
+    file.write("\n".as_bytes())
+}
+
+fn define_visitor<'a>(file: &mut File, base_name: &str, types: &Vec<GrammarTy>) -> IORes {
+    file.write(format!("pub trait Visitor<R> {{\n").as_bytes())?;
+    for ty in types {
         file.write(
             format!(
-                "    fn visit_{}_{}(&self, {}: &{}) -> R;\n",
-                class.to_lowercase(),
+                "{:4}fn visit_{}_{}(&self, {}: &{}) -> R;\n",
+                " ",
+                ty.class_name.to_lowercase(),
                 base_name.to_lowercase(),
-                class.to_lowercase(),
-                class,
+                ty.class_name.to_lowercase(),
+                ty.class_name,
             )
             .as_bytes(),
-        )
-        .unwrap();
+        )?;
     }
-    file.write("}".as_bytes()).unwrap();
+    file.write("}".as_bytes())
 }
 
 mod ty_map {
@@ -152,6 +140,36 @@ mod ty_map {
     }
 }
 
+struct TypeName<'a> {
+    ty: &'a str,
+    name: &'a str,
+}
+
+struct GrammarTy<'a> {
+    class_name: &'a str,
+    fields: Vec<TypeName<'a>>,
+}
+
+fn parse_types(grammar: &Vec<String>) -> Vec<GrammarTy> {
+    let mut vec = vec![];
+    for entry in grammar {
+        let mut s = entry.split(":");
+        let class_name = s.next().unwrap().trim();
+        let fields = s.next().unwrap().split(",");
+        let fields = fields
+            .map(|f| {
+                let mut field = f.trim().split(" ");
+                let ty = field.next().unwrap().trim();
+                let name = field.next().unwrap().trim();
+                TypeName { ty, name }
+            })
+            .collect();
+        vec.push(GrammarTy { class_name, fields });
+    }
+
+    vec
+}
+
 fn main() {
     let out_dir = generate_ast();
 
@@ -162,5 +180,7 @@ fn main() {
         "Unary    : Token operator, Expr right".to_string(),
     ];
 
-    define_ast(&out_dir, "Expr", v);
+    let types = parse_types(&v);
+
+    define_ast(&out_dir, "Expr", &types).unwrap();
 }
